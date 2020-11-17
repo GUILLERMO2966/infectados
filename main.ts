6674006408113091b18f885b056cac272bc9dfcd
GUILLERMO2966 <71560170+GUILLERMO2966@users.noreply.github.com>
2020-11-17 14:44:43 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,8 @@ let Recuperados = 0
 let Tasa_Interaccion = 2
 let Probabilidad_Contagio = 0.1
 basic.forever(function () {
-    Contagios = Infectados * (Tasa_Interaccion * (Suceptibles / (Infectados + (Suceptibles + Recuperados * Probabilidad_Contagio))))
+    Contagios = Infectados * (Tasa_Interaccion * (Suceptibles / (Infectados + (Suceptibles + Recuperados))))
+    Contagios = Contagios * Probabilidad_Contagio
     basic.showString("CONTAGIOS")
     basic.showNumber(Contagios)
 })
